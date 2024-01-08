b4f1535f5bf38ca2abe67aea6303f18cbb1d5e44
protarie125 <protarie@gmail.com>
2024-01-08 12:10:21 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <bit>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+using vs = vector<string>;
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+
+  auto ans = vs{};
+  for (auto i = 0; i < s.length(); ++i) {
+    ans.push_back(s.substr(i));
+  }
+
+  sort(ans.begin(), ans.end());
+  for (const auto& x : ans) {
+    cout << x << '\n';
+  }
+
+  return 0;
+}
